e58b4b4ff4d50c3fff5a41782a9379475f331259
TheValour <helloworldexe18@gmail.com>
2024-04-21 20:07:36 +0530

    adding basic structure + assets

--- app/index.js
+++ app/index.js
@@ -1,20 +1,14 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center">
       <Text>Mobo</Text>
       <StatusBar style="auto" />
+      <Link href="/profile">Profile Section</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
